4f003c0eb95fded11691e3d3ec5a07c2674ce82b
Abhijeet Prasad <abhi.prasad16@gmail.com>
2020-05-17 02:23:41 +0530

    update

--- update.php
+++ update.php
@@ -1,18 +1,31 @@
 <?php 
 session_start();
 // Verify User ID
+
+$server = "localhost";
+$user = "dimun_sec";
+$pass = "";
+$dbname = "dimun";
+$mysqli = new mysqli($server, $user, $pass, $dbname);
+ 
+//Checking connection
+ 
+if($mysqli->connect_error){
+ die("Connection failed:" . $mysqli->connect_error);
+}
 $userid = $_SESSION['uid'];
 $fname=$_POST['first_name'];
 $lname=$_POST['last_name'];
 $email=$_POST['email'];
 $mobile=$_POST['mobile-number'];
 // Update data
-$update = "UPDATE register SET first_name='$fname',last_name='$lname', email ='$email',mobile'$mobile' WHERE uid ='$userid";
-$update_query = mysqli->query($update);
-if($update_query){
-	header("Location: user-profile.php);
+$q = "UPDATE register SET first_name='$fname',last_name='$lname', email ='$email',mobile='$mobile' WHERE uid ='$userid'";
+
+if($mysqli->query($q) === TRUE){
+	session_destroy();
 	}
 else {
 	echo "Error";
 }
+$mysqli->close();
 ?>
